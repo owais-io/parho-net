398bc3b7098eb66991315719b40fbb510cd5f7fa
owais-io <owais.abbasi9@gmail.com>
2025-08-16 22:09:47 +0500

    Add Vercel Analytics and Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+// pages/_app.tsx
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({
   Component,
@@ -9,6 +13,8 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </SessionProvider>
   )
 }
